d93dcd26e6215db3673349d9605b2f0ef5af9683
devanshi mishra <devu2732@gmail.com>
2021-04-22 18:12:39 +0530

    commit

--- prog1.cpp
+++ prog1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main( ){
     int n , sum=0;
-    cout<<"Enter value of n";
+    cout<<"Enter value of n\n";
     cin>>n;
     for(int i=1;i<=n;i++)
     {
         sum=sum+i;
         }
-    cout<<"sum of n natural no's="<<sum;
+    cout<<"sum of n natural no's=\n"<<sum;
 }
