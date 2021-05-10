07f35ad6d0bf908672818ef8e7e04547daf71d3d
devanshi mishra <devu2732@gmail.com>
2021-05-10 22:02:22 +0530

    commit

---
+++ prog3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,table;
+    cout<<"enter any no\n";
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+      table=n*i;
+      cout<<"\n"<<table;
+    }
+}
