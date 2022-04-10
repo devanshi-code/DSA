34c90c3e52aa8c7ea659c667bceb678936facfa5
devanshi-code <devu2732@gmail.com>
2022-04-10 23:20:07 +0530

    commit

---
+++ HASHING/basicprogram.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+#include<map>
+#include<utility>
+using namespace std;
+int main()
+{
+    map<int,string> Students;
+    Students[1]="Devanshi";
+    Students[2]="Shruti";
+    Students[3]="Sachin";
+    Students[4]="Rupali";
+    cout<<"Students[3]"<<Students[3]<<endl;
+    cout<<"Maps.size:"<<Students.size()<<endl;
+    for(map<int,string>::iterator i=Students.begin();i!=Students.end();++i)
+    {
+        cout<<(*i).first<<" "<<(*i).second<<endl;
+    }
+     cout << "\nReverse Order:" << endl;  
+    for( map<int,string>::reverse_iterator i=Students.rbegin(); i!=Students.rend(); ++i)  
+{  
+    cout << (*i).first << ": " << (*i).second << endl;  
+}
